03c5168fd6061e2a026f3df79960293c196953dc
Kadacheahmedrami <a_kadache@estin.dz>
2025-06-01 01:59:53 +0200

    adding padding to the ehader

--- web/components/header.tsx
+++ web/components/header.tsx
@@ -16,7 +16,7 @@ export function Header() {
   ]
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="p-1 px-4 sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-auto container flex h-16 max-w-screen-2xl items-center justify-between">
         <Link href="/" className="flex items-center space-x-2">
           <Bot className="h-6 w-6 text-primary" />
